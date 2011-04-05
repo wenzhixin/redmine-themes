e6bc1fcc211ed45dc4e4496c017c51fa4c1d7cf3
baucher@gmail.com <baucher@gmail.com@1ff8e794-542d-11de-907f-3386865dedc4>
2011-04-05 18:42:25 +0000

    If a history item contains a comment and another item, then hide now the item.

--- plugins/redmine_issue_history_tabs/init.rb
+++ plugins/redmine_issue_history_tabs/init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_issue_history_tabs do
   name 'Redmine Issue History Tabs plugin'
-  author 'Konstantin Zaitsev'
+  author 'Konstantin Zaitsev - Marc Baucher'
   description 'This plugin provide history of issue in tabs representation'
-  version '0.0.3'
+  version '0.0.4'
 end
