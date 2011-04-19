16164a2d4dd303e1c7280665ec24ef3796d83ce2
baucher@gmail.com <baucher@gmail.com@1ff8e794-542d-11de-907f-3386865dedc4>
2011-04-19 20:05:38 +0000

    Fix issue #3

--- plugins/redmine_issue_history_tabs/init.rb
+++ plugins/redmine_issue_history_tabs/init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :redmine_issue_history_tabs do
   name 'Redmine Issue History Tabs plugin'
   author 'Konstantin Zaitsev - Marc Baucher'
   description 'This plugin provide history of issue in tabs representation'
-  version '0.0.4'
+  version '0.0.5'
 end
