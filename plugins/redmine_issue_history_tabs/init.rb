0bdcd0983cd0abdd981fe23ffdc345fce166cf2e
zhixin <wenzhixin2010@gmail.com>
2016-10-25 19:36:52 +0800

    Remove plugins

--- plugins/redmine_issue_history_tabs/init.rb
+++
@@ -1,8 +0,0 @@
-require 'redmine'
-
-Redmine::Plugin.register :redmine_issue_history_tabs do
-  name 'Redmine Issue History Tabs plugin'
-  author 'Konstantin Zaitsev - Marc Baucher'
-  description 'This plugin provide history of issue in tabs representation'
-  version '0.0.5'
-end
